f4e516fb6d58779e4a0a8ec63029fbcd8321c015
Eztas <nietzsche2669sensei@gmail.com>
2025-12-14 18:10:38 +0900

    change: comment: 数値調整とコメント追加

--- app/components/MapComponent.tsx
+++ app/components/MapComponent.tsx
@@ -7,16 +7,18 @@ import L from "leaflet";
 import { useEffect, useState } from "react";
 
 // Propsの型定義
-type Props = {
+type MapComponentProps = {
   lat: number;
   lng: number;
   targetTime: string; // "1430" 形式
 };
 
-// ▼ Leafletのアイコンバグ修正
+// Leafletのアイコンバグ修正
 const fixLeafletIcon = () => {
   // @ts-ignore
   delete L.Icon.Default.prototype._getIconUrl;
+  // デフォルトの画像パス読み込みを削除し、CDN上の画像URLを取得
+  // 「マーカー画像が表示されず壊れる」という有名なバグを直す
   L.Icon.Default.mergeOptions({
     iconRetinaUrl: "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png",
     iconUrl: "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png",
@@ -29,12 +31,12 @@ const fixLeafletIcon = () => {
 const MapUpdater = ({ center }: { center: [number, number] }) => {
   const map = useMap();
   useEffect(() => {
-    map.flyTo(center, map.getZoom()); // ヌルっと移動
+    map.flyTo(center, map.getZoom()); // 現在のzoom値に合わせてマップをレンダリング
   }, [center, map]);
   return null;
 };
 
-const MapComponent = ({ lat, lng, targetTime }: Props) => {
+const MapComponent = ({ lat, lng, targetTime }: MapComponentProps) => {
   const centerPos: [number, number] = [lat, lng];
   const [radius, setRadius] = useState(500); // デフォルト半径
 
@@ -55,8 +57,8 @@ const MapComponent = ({ lat, lng, targetTime }: Props) => {
     const diffSeconds = (target.getTime() - now.getTime()) / 1000;
     if (diffSeconds > 0) {
       const remainingMinutes = diffSeconds / 60;
-      const calcRadius = remainingMinutes * 80; 
-      setRadius(Math.max(calcRadius, 50)); // 最低50mは確保
+      const calcRadius = remainingMinutes * 20; 
+      setRadius(calcRadius); // 最低50mは確保
     } else {
       setRadius(0); // 時間切れ
     }
@@ -65,11 +67,12 @@ const MapComponent = ({ lat, lng, targetTime }: Props) => {
   return (
     <MapContainer
       center={centerPos}
-      zoom={16} // 少し引いて全体を見やすく
+      zoom={16}
       style={{ width: "100%", height: "100%" }}
     >
       <MapUpdater center={centerPos} />
       
+      {/* OpenStreetMapのタイル使用時の番号とズーム値, クレジット, leaflet側でユーザー操作に合わせて動的に変更できる */}
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
