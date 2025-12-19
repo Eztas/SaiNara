a6465a01892155cd796f9e49403ba6945fede22e
shunsei <k.shunsei1227@gmail.com>
2025-12-20 01:15:13 +0900

    feat: add enjoy map button

--- app/enjoy/page.tsx
+++ app/enjoy/page.tsx
@@ -6,8 +6,8 @@ import dynamic from "next/dynamic";
 import { useSearchParams } from "next/navigation";
 import { Suspense } from "react";
 
-import { parseCoords, parseTime } from "@/lib/validation"
 import { MapChange } from "@/components/MapChange";
+import { parseCoords, parseTime } from "@/lib/validation";
 
 // 地図コンポーネント（ssr: falseでSSR無効化）
 const BaseMap = dynamic(() => import("@/components/map/BaseMap"), {
@@ -46,7 +46,7 @@ function MapContent() {
       <BaseMap>
         <EnjoyMap />
       </BaseMap>
-      <MapChange lat={lat} lng={lng} targetTime={targetTime} />
+      <MapChange targetTime={targetTime} />
     </>
   );
 }
